2126e0fbd630d82f2b6c779261d6628f738c2c55
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2026-03-04 19:50:32 +0300

    Added the test run to main

--- tests/test_bst.c
+++ tests/test_bst.c
@@ -11,6 +11,7 @@ void testNullHandling();
 void testMultipleOperations();
 void testHeightAndSize();
 void testMinAndMax();
+void testMerge();
 
 // Запуск всех тестов
 int main()
@@ -25,6 +26,7 @@ int main()
     testMultipleOperations();
     testHeightAndSize();
     testMinAndMax();
+    testMerge();
 
     puts("\nAll test passed!");
     return 0;
