a2dad8342b8f5f2f2142d67fb5a4979972bb67d4
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2026-03-04 21:37:05 +0300

    Small fix

--- src/bst.c
+++ src/bst.c
@@ -37,7 +37,10 @@ static BSTNode* insertRec(BSTNode* node, int value)
 void bstInsert(BST* tree, int value)
 {
     if (tree != NULL) {
-        tree->root = insertRec(tree->root, value);
+        BSTNode* newRoot = insertRec(tree->root, value);
+        if (newRoot != NULL) {
+            tree->root = newRoot;
+        }
     }
 }
 
